e4d9f2598c1070d4cd84ce5279bf5d02f2ef50b4
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-03-12 16:49:14 +0530

    Create multiples.c

---
+++ c programming/multiples.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int i=1;
+   int n,p;
+       scanf("%d",&p);
+   while(i<=10){
+    n=p*i;
+       printf("%d\n", n);
+       i++;
+       
+   }
+    return 0;
+}
