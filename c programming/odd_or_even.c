791e8c43511a90dd2eb7edb283f22cad20ea1761
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-03-12 14:21:05 +0530

    Create odd_or_even.c

---
+++ c programming/odd_or_even.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int x;
+    scanf("%d",&x);
+    
+    if(x%2==1){
+        printf("the number is odd");
+    }
+    else{
+        printf("the number is even");
+    }
+ 
+    return 0;   
+}
