0cb052dba0028b8aef0b136717de06052c8e9ca4
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-04-04 20:47:35 +0530

    Create pattern.c

---
+++ c programming/pattern.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    char mychar='*';
+    for(int j=1;j<=6;j++){
+        
+    for(int i=0;i<=5;i++){
+        printf("%c",'*');
+    }
+    printf("\n");
+    }
+    return 0;
+}
