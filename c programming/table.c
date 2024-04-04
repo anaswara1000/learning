34be75f2fe16ffb769226846010a38d8aa63bb7a
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-04-04 18:40:19 +0530

    Create table.c

---
+++ c programming/table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    
+    int num,i,mul;
+    scanf("%d",&num);
+    for(i=1;i<=10;i++){
+        mul=num*i;
+        printf("%d\n",mul);
+    }
+    
+    
+    return 0;
+}
