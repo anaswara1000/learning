badbb7518557767786a6b2a75062d5ddd79e23ca
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-05-01 19:09:17 +0530

    Update strcmp.c

--- c programming/strcmp.c
+++ c programming/strcmp.c
@@ -1,19 +1,27 @@
-#include <stdio.h>
 #include<string.h>
 int main()
 {
-   int value;
+   int flag=0,i;
    char s1[30],s2[30];
    printf("string s1:");
-   scanf("%s",s1);
+   scanf("%[^\n]s",s1);
+   getchar();
     printf("string s2:");
-    scanf("%s",s2);
-value=strcmp(s1,s2);
-if(value==0){
+    scanf("%[^\n]s",s2);
+    getchar();
+for(i=0;s1[i]!='\0'|| s2[i]!='\0';i++){
+if(s1[i]!=s2[i]){
+    flag=1;
+    break;
+        }
+}
+    
+    if (flag==0){
     printf("same");
 }
 else{
     printf("not same");
 }
+    
     return 0;
 }
