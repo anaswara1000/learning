d8400bce835ac16d1cbdd2aaa59b4d1ea99f8341
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-05-15 18:15:16 +0530

    Create madlibsgame.py

---
+++ python/madlibsgame.py
@@ -0,0 +1,9 @@
+
+colour=input("enter a colour:")
+plural_noun=input("enter a plural noun:")
+celebrity=input("enter a celebrity:")
+
+
+print("roses are "+colour)
+print(plural_noun+" are blue")
+print("i love "+celebrity)
