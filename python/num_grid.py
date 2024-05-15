1a12fce789931de7ba10c8da0def50eb2277e93a
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-05-15 18:23:42 +0530

    Create num_grid.py

---
+++ python/num_grid.py
@@ -0,0 +1,16 @@
+num_grid=[
+    
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+    
+    ]
+    
+print(num_grid[2][0])
+
+for row in num_grid:
+    for col in row:
+       # print(row)
+        print(col)
+    
