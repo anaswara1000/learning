15f1c12b4888af7059f722f1f795a042c8140b30
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-05-15 18:19:28 +0530

    Create calculator.py

---
+++ python/calculator.py
@@ -0,0 +1,15 @@
+num1=float(input("enter no: "))
+op=input("enter op: ")
+num2=float(input("enter no: "))
+
+if op=="+":
+    print(num1+num2)
+elif op=="-":
+    print(num1-num2)
+elif op=="*":
+    print(num1*num2)
+elif op=="/":
+    print(num1/num2)
+else:
+    print("invalid")
+    
