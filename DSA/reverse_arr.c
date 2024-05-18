6f633e29927c9b393fc0c267400dd1ffd1161148
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-05-18 17:52:36 +0530

    Create reverse_arr.c

---
+++ DSA/reverse_arr.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct array {
+    int A[20];
+    int size;
+    int len;
+};
+
+void display(struct array arr){
+   int i;
+   for(i=0;i<arr.len;i++)
+       printf("%d ",arr.A[i]);
+   
+}
+
+ void rev(struct array *arr){
+     int *B;
+     int i,j;
+     B=(int *)malloc(arr->len*sizeof(int));
+     for(i=arr->len-1,j=0;i>=0;i--,j++)
+     B[j]=arr->A[i];
+     for(i=0;i<arr->len;i++)
+     arr->A[i]=B[i];
+     
+ }
+ 
+ 
+    int main(){
+        struct array arr={{2,3,4,5,6},10,5};
+  
+        rev(&arr);
+        display(arr);
+       
+        return 0;
+    }
