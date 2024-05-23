ed86a68dd85c7afddb367a0374ec6b28c8c61a31
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-05-23 08:55:10 +0530

    Update power(exponent)_rec.c

--- DSA/Recursion/power(exponent)_rec.c
+++ DSA/Recursion/power(exponent)_rec.c
@@ -10,16 +10,26 @@ int power(int m,int n){
         return power(m,n-1)*m;
     }
 }
-
-
+/////////////////another method
+int power1(int m,int n){
+    if(n==0)
+        return 1;
+    if(n%2==0)
+        return power1(m*m,n/2);
+    else
+        return m*pow(m*m,(n-1)/2);
+}
 
 
 int main(){
     int m=2;
     int n=3;
-    printf("%d",power(2,9));
+    printf("%d",power1(2,9));
     return 0;
 }
+
+
+
 
 
 
