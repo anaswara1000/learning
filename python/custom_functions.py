6cf2fda98e747895a37bfe0ba7d7522f526ffa33
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-05-27 23:29:22 +0530

    Create custom_functions.py

---
+++ python/custom_functions.py
@@ -0,0 +1,34 @@
+def greet():
+    message="hello"
+    new_msg=message.capitalize()
+    print("hey hey")
+    return new_msg
+
+greeting = greet()
+print(greeting)
+
+# output:hey hey
+# Hello
+# ##############################################
+def greet():
+    message="hello"
+    new_msg=message.capitalize()
+    print("hey hey")
+  
+
+greeting = greet()
+print(greeting)
+
+# output:hey hey
+None
+# ###################################################
+def greet():
+    message="hello"
+    new_msg=message.capitalize()
+    print("hey hey")
+
+
+greeting = greet()
+
+# output:hey hey
+###################################################
