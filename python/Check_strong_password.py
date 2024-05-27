1763f88cec3937d1375350a67bf161492ec7efc7
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-05-27 18:44:59 +0530

    Create Check_strong_password.py

---
+++ python/Check_strong_password.py
@@ -0,0 +1,29 @@
+password=input("enter new password: ")
+
+result = {}
+
+if len(password)>=8:
+    result["length"]=True
+else:
+    result["length"] = False
+digit = False
+for i in password:
+    if i.isdigit():
+        digit=True
+
+result["digits"]=digit
+
+
+uppercase= False
+for i in password:
+    if i.isupper():
+        uppercase=True
+
+result["uppercase"]=uppercase
+
+print(result)
+
+if all(result.values())==True:
+    print("Strong Password")
+else:
+    print("Weak Password")
