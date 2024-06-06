aff4f6f081a49fb5155ba3aae61976c5f13a0bc9
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-06-06 21:07:03 +0530

    Update striver_stack_overflow.cpp

--- DSA/Recursion/striver_stack_overflow.cpp
+++ DSA/Recursion/striver_stack_overflow.cpp
@@ -10,3 +10,23 @@ int main(){
     print();
     return 0;
 }
+
+// //////////////////////////////////
+#include<iostream>
+using namespace std;
+
+ int  count=0;
+void print(){
+    if(count==4)
+    return;
+    else
+    cout<<count<<endl;
+    
+    count++;
+    print();
+}
+
+int main(){
+    print();
+    return 0;
+}
