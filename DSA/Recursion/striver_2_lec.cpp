f42250949ec1bae046b1608fe247d1cd0c34fcda
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-06-07 12:20:50 +0530

    Update striver_2_lec.cpp

--- DSA/Recursion/striver_2_lec.cpp
+++ DSA/Recursion/striver_2_lec.cpp
@@ -19,3 +19,48 @@ int main(){
     f(1,n);
     return 0;
 }
+
+// print 1234 linearly
+
+#include<iostream>
+using namespace std;
+
+void f(int i,int n){
+    if(i>n)
+    return;   //base condition
+    
+  cout<<i;
+   f(i+1,n);
+    
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    f(1,n);
+    return 0;
+}
+
+//print 4321
+
+#include<iostream>
+using namespace std;
+
+void f(int i,int n){
+    if(i<n)
+    return;   //base condition
+    
+  cout<<i;
+   f(i-1,n);
+    
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    f(n,1);
+    return 0;
+}
+
