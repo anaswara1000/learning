600caecf36568dbbbc2d19c12cc0ba43e2f4daa4
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-06-07 14:39:32 +0530

    Update striver_3_lec.cpp

--- DSA/Recursion/striver_3_lec.cpp
+++ DSA/Recursion/striver_3_lec.cpp
@@ -15,3 +15,19 @@ int main()
    cout<<sum(n);
     return 0;
 }
+//factorial
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if(n-1==0) return n;
+    return n * fact(n-1);
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<fact(n);
+    
+}
