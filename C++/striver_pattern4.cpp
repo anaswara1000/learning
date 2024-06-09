58fc4575ca0e6984bf07bec64fe64b8fae6e1ac7
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-06-10 00:27:48 +0530

    Update striver_pattern4.cpp
    5
    12345
    1234
    123
    12
    1

--- C++/striver_pattern4.cpp
+++ C++/striver_pattern4.cpp
@@ -25,3 +25,30 @@ int main()
 ***
 **
 *
+
+
+    #include <iostream>
+using namespace std
+;
+int main()
+{
+    int i,j=0,n;
+    cin>>n;
+  
+   for(i=0;i<=n;i++){
+       for(j=1;j<n-i+1;j++){
+           cout<<j<<"";
+       
+       }
+       cout<<endl;
+   }
+
+    return 0;
+}
+
+5
+12345
+1234
+123
+12
+1
