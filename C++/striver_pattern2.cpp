e260ef4b86ca3390b559275d71e86202e68f0fee
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-06-10 00:07:54 +0530

    Update striver_pattern2.cpp

--- C++/striver_pattern2.cpp
+++ C++/striver_pattern2.cpp
@@ -23,3 +23,29 @@ int main()
 ***
 ****
 *****
+
+    #include <iostream>
+using namespace std
+;
+int main()
+{
+    int i,j=0,n;
+    cin>>n;
+    int x=1;
+   for(i=1;i<n;i++){
+       for(j=1;j<=i;j++){
+           cout<<j<<"";
+       
+       }
+       cout<<endl;
+   }
+
+    return 0;
+}
+
+
+5
+1
+12
+123
+1234
