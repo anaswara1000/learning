3f4f633dec419ebbc97ddc37eca5991197dc3c49
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-06-10 14:31:39 +0530

    Create hashing.cpp

---
+++ C++/hashing.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,j,n;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+      
+    }
+    
+// precompute
+
+   int hash[13]={0};
+   for(i=0;i<n;i++){
+       hash[arr[i]]+=1;
+   }
+  
+  int q;
+  cin>>q;
+  while(q--){
+      int number;
+      cin>>number;   //fetch
+      
+      cout<<hash[number]<<endl;
+  }
+
+    return 0;
+}
