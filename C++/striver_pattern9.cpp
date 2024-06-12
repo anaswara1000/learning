477977af4b6ab245558a9b9de013c6f1af080ea9
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-06-12 23:56:10 +0530

    Create striver_pattern9.cpp

---
+++ C++/striver_pattern9.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+    int space = 2*(n-1);
+    for(int i=1;i<=n;i++){
+        
+        for(int j=1;j<=i;j++)
+        cout<<j;
+        
+        for(int j=1;j<=space;j++)
+        cout<<" ";
+        
+         for(int j=i;j>=1;j--)
+        cout<<j;
+        
+    cout<<endl;
+    space-=2;
+    
+    }
+}
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int n;
+        cin>>n;
+        print1(n);
+    }
+
+}
+
+
+1
+5
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+
+
