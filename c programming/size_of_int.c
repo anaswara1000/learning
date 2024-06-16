5a71f1ce6a0c5e2abaddca461b365a64ba35a3b6
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-06-16 17:40:57 +0530

    Create size_of_int.c

---
+++ c programming/size_of_int.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    char c;
+    float f;
+    double d;
+    printf("size of int:%ld\n",sizeof(i));
+     printf("size of char:%ld\n",sizeof(c));
+     printf("size of float:%ld\n",sizeof(f));
+      printf("size of double:%ld\n",sizeof(d));
+   
+
+    return 0;
+}
