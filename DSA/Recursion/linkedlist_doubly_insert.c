da21008280f8c74bc1dbeb0f0703a01111fbdcea
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-06-19 19:34:01 +0530

    Create linkedlist_doubly_insert.c

---
+++ DSA/Recursion/linkedlist_doubly_insert.c
@@ -0,0 +1,87 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node{
+    struct node *prev;
+    int data;
+    struct node *next;
+}
+*first=NULL;
+
+void create(int a[],int n){
+    struct node *t,*last;
+    int i;
+    first=(struct node*)malloc(sizeof(struct node));
+    first->data=a[0];
+    first->prev=NULL;
+    first->next=NULL;
+    last=first;
+    
+    for(i=1;i<n;i++){
+        t=(struct node *)malloc(sizeof(struct node));
+        t->data=a[i];
+        t->next=last->next;
+        t->prev=last;
+        last->next=t;
+        last=t;
+    }
+}
+
+int length(struct node *p){
+    int len =0;
+    while(p){
+        len++;
+        p=p->next;
+    }
+    return len;
+}
+
+
+void insert(struct node*p,int index,int x){
+    struct node *t;
+    int i;
+    
+    if(index<0 || index>length(p))
+    return;
+    if(index==0){
+        t=(struct node *)malloc(sizeof(struct node));
+        t->data=x;
+        t->prev=NULL;
+        t->next=first;
+        first=t;
+    }
+    else{
+        for(i=0;i<index-1;i++)
+        p=p->next;
+        t=(struct node *)malloc(sizeof(struct node));
+        t->data=x;
+        t->next=p->next;
+        t->prev=p;
+        if(p->next)
+        p->next->prev=t;
+        p->next=t;
+        
+    }
+}
+
+
+
+void display(struct node *p){
+    while(p){
+        printf(" %d",p->data);
+        p=p->next;
+    }
+    
+}
+
+
+int main(){
+    int a[]={10,20,30,40,50};
+    create(a,5);
+    insert(first,2,25);
+    display(first);
+    return 0;
+}
+
+
