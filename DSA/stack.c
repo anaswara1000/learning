14ffd3e2e42708f6b16076c73db38365eb0aff15
anaswara1000 <157507387+anaswara1000@users.noreply.github.com>
2024-06-24 01:05:05 +0530

    Update stack.c

--- DSA/stack.c
+++ DSA/stack.c
@@ -1,46 +1,47 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 
+
 struct stack{
-    int size;
+   
     int top;
-    int s[];
+    int *s;
+    int size;
 };
 
 void push(struct stack *st,int x){
-    if(st->top==(st->size)-1){
-        printf("stack overflow\n");
+    if(st->top==st->size-1){
+        printf("stack overflow");
     }
     else{
-        (st->top)++;
+        st->top++;
         st->s[st->top]=x;
     }
-}    
+}
 
 int pop(struct stack *st){
     int x=-1;
     if(st->top==-1){
-        printf("stack underflow\n");
+        printf("stack underflow");
     }
     else{
         x=st->s[st->top];
         st->top--;
     }
     return x;
+
 }
 
-int main()
-{
+int main(){
     struct stack *st=malloc(sizeof(struct stack));
-    scanf("%d",&(st->size));
+    scanf("%d",&st->size);
+    st->s=malloc(st->size*sizeof(int));
     st->top=-1;
+    push(st,3);
+    push(st,4);
     push(st,5);
-    push(st,7);
-    push(st,8);
-     pop(st);
+    pop(st);
     for(int i=0;i<=st->top;i++){
         printf("%d ",st->s[i]);
     }
-    free(st);
-    return 0;
 }
