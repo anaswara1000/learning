64a1720452db96f80d76dd0f237c0e34f2dcb305
Anaswara R <157507387+anaswara1000@users.noreply.github.com>
2024-08-04 19:22:47 +0530

    Create dumb.cpp

---
+++ C++/dumb.cpp
@@ -0,0 +1,236 @@
+2
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello world"<<endl;
+    return 0;
+}
+
+
+6
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    cout<<"enter two numbers:"<<endl;
+    int n1=0,n2=0;
+    cin>>n1>>n2;
+    cout<<"sum of "<<n1<<" and "<<n2<<" is "<<n1+n2<<endl;
+    return 0;
+}
+
+10
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+  int count=0;
+      
+  while(count<5){
+      cout<<"hey there"<<endl;
+      count+=1;
+  }
+    return 0;
+}
+
+
+10.1
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+  int count=1,sum=0,val;
+     cin>>val;
+  while(count<=val){
+      sum+=count;
+      count+=1;
+  }
+     cout<<sum<<endl;
+    return 0;
+}
+
+11
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum=0;
+    int n=0;
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        sum+=i;
+    }
+    cout<<sum<<endl;
+}
+
+12
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+  int sum=0,val=0;
+   
+  while(cin>>val)
+      sum+=val;
+     cout<<sum<<endl;
+    return 0;
+}
+
+13
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n=0;
+  cin>>n;
+  if(n>15){
+      cout<<"greater"<<endl;
+  }
+  else{
+      cout<<"lesser"<<endl;
+  }
+ 
+    return 0;
+}
+
+2
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"hello world"<<endl;
+    return 0;
+}
+
+
+6
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    cout<<"enter two numbers:"<<endl;
+    int n1=0,n2=0;
+    cin>>n1>>n2;
+    cout<<"sum of "<<n1<<" and "<<n2<<" is "<<n1+n2<<endl;
+    return 0;
+}
+
+10
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+  int count=0;
+      
+  while(count<5){
+      cout<<"hey there"<<endl;
+      count+=1;
+  }
+    return 0;
+}
+
+
+10.1
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+  int count=1,sum=0,val;
+     cin>>val;
+  while(count<=val){
+      sum+=count;
+      count+=1;
+  }
+     cout<<sum<<endl;
+    return 0;
+}
+
+11
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int sum=0;
+    int n=0;
+    cin>>n;
+    for(int i=0;i<=n;i++){
+        sum+=i;
+    }
+    cout<<sum<<endl;
+}
+
+12
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+  int sum=0,val=0;
+   
+  while(cin>>val)
+      sum+=val;
+     cout<<sum<<endl;
+    return 0;
+}
+
+13
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+  int n=0;
+  cin>>n;
+  if(n==15){
+      cout<<"equal"<<endl;
+  }
+  else if(n<15){
+      cout<<"lesser"<<endl;
+  }
+    else{
+        cout<<"greater"<<endl;
+    }
+  
+    return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   int n=0;
+   cin>>n;
+   if(n==15){
+       cout<<"equal"<<endl;
+   }
+   else if(n<15){
+       cout<<"lesser"<<endl;
+   }
+    else{
+        cout<<"greater"<<endl;
+    }
+  
+    return 0;
+} 
